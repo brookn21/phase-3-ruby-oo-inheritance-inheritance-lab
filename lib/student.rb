9212057c222f022827b462871c5b4c02e0b37695
Brook Nega <brooknega21@gmail.com>
2022-11-07 13:47:30 -0500

    commit tree 99cbaa21b7564fe8c405eab71b4a63100697941c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user' 
 
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        @knowledge = []
+    end
+
+    def learn subject
+        @knowledge << subject
+    end
+
+    def knowledge 
+        return @knowledge
+    end
+    
 end
+
